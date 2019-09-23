5a07f023f884f20f679fcdf58745762a111ae4f8
Ian van den Heuvel <blueangelcpt@gmail.com>
2019-09-23 08:52:41 +0200

    Fixes to ATC component.

--- Controller/Component/AtcComponent.php
+++ Controller/Component/AtcComponent.php
@@ -2,7 +2,6 @@
 App::uses('HttpSocket', 'Network/Http');
 class AtcComponent extends Component {
 	public $settings = array(
-		'url' => 'https://clientserv.net',
 		'TradeCode' => '',
 		'TradePass' => '',
 		'TerminalId' => ''
@@ -17,38 +16,44 @@ class AtcComponent extends Component {
 	}
 
 	public function purchase($meterNumber, $amount) {
-		$request = array(
+		$payload = array(
+			'TradeCode' => $this->settings['TradeCode'],
+			'TradePass' => $this->settings['TradePass'],
+			'TerminalId' => $this->settings['TerminalId']
+		);
+		// $result = $this->socket->post('https://clientserv.net/ATCMerchants/api/Elec?MeterNumber=' . $meterNumber . '&Amount=' . ($amount / 100), json_encode($payload, true), $request);
+		$result = $this->socket->request(array(
+			'method' => 'POST',
+			'uri' => 'https://clientserv.net/ATCMerchants/api/Elec?MeterNumber=' . $meterNumber . '&Amount=' . ($amount / 100),
+			'body' => json_encode($payload, true),
 			'header' => array(
 				'Content-Type' => 'application/json',
 				'Connection' => 'keep-alive',
 				'Cache-Control' => 'no-cache'
 			)
-		);
-		$payload = array(
-			'TradeCode' => $this->settings['TradeCode'],
-			'TradePass' => $this->settings['TradePass']
-		);
-		$url = 'api/Elec?MeterNumber=' . $meterNumber . '&Amount=' . ($amount / 100);
-		$result = $this->socket->post($this->settings['url'] . $url, $payload, $request);
+		));
 		$this->log('ATC API purchase request: ' . $this->socket->request['raw'], $this->tag);
 		$this->log('ATC API purchase response (N$' . ($amount / 100) . ' for ' . $meterNumber . '): ' . $result, $this->tag);
 		return json_decode($result->body, true);
 	}
 
 	public function validate($meterNumber) {
-		$request = array(
+		$payload = array(
+			'TradeCode' => $this->settings['TradeCode'],
+			'TradePass' => $this->settings['TradePass'],
+			'TerminalId' => $this->settings['TerminalId']
+		);
+		// $result = $this->socket->post('https://clientserv.net/ATCMerchants/api/Elec?MeterNumber=' . $meterNumber, json_encode($payload, true), $request);
+		$result = $this->socket->request(array(
+			'method' => 'GET',
+			'uri' => 'https://clientserv.net/ATCMerchants/api/Elec?MeterNumber=' . $meterNumber,
+			'body' => json_encode($payload, true),
 			'header' => array(
 				'Content-Type' => 'application/json',
 				'Connection' => 'keep-alive',
 				'Cache-Control' => 'no-cache'
 			)
-		);
-		$payload = array(
-			'TradeCode' => $this->settings['TradeCode'],
-			'TradePass' => $this->settings['TradePass']
-		);
-		$url = 'api/Elec?MeterNumber=' . $meterNumber;
-		$result = $this->socket->post($this->settings['url'] . $url, $payload, $request);
+		));
 		$this->log('ATC API validation request: ' . $this->socket->request['raw'], $this->tag);
 		$this->log('ATC API validation response (' . $meterNumber . '): ' . $result, $this->tag);
 		return json_decode($result->body, true);
